179730e4a638f7a4b4987a1a93d1475b7500b5dd
ktorres63 <ktorresaro@unsa.edu.pe>
2024-11-17 00:58:51 -0500

    fix quasar config

--- quasar.config.js
+++ quasar.config.js
@@ -47,8 +47,8 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
-      distDir:'dir',
-      
+      distDir:'dist',
+
       target: {
         browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
         node: 'node20'
